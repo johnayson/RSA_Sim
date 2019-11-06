7d08c1d8d21eb049b7a5314eefb31659669f245f
John <jdayson@ucdavis.edu>
2019-11-06 00:08:25 -0800

    Cleaned up

--- RSA3.c
+++ RSA3.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
+int gcd (int a,int b);
 char * lettersToBinary(char recv);
-void  blocker (char * toBlock);
+int *  blocker (char * toBlock);
+//int gcd (int a,int b);
+
+//RSA formula values
+double e = 7; //public key
+double p = 3;  //prime #1
+double q = 5; //prime #2
+int int_blocks[10]; //integer blocks
+int int_blk_size = 0; //how many integer blocks are use
 
 //%c for char %d for ints
 int main(void)
@@ -25,21 +35,14 @@ int main(void)
 		char *retCharPtr;
 		int j;
 		retCharPtr = lettersToBinary(*charPtr);
-		//printf("%s\n",retCharPtr );
-		//printf("%d\n", strlen(retCharPtr));
-		//print("%c\n", numEncode);
 		int stopper = counter +strlen(retCharPtr);
 		for(j = counter; j < stopper; j++)
 		{
 			encodedMessage[j] = *retCharPtr;
-			//when you print the first one its prints the hwole thing but you only inserted the first one
-			//printf("%s\n", retCharPtr);
 			retCharPtr++;
 		}
 
 		counter = j;
-		//strcpy(encodedMessage[i], lettersToBinary(*charPtr));
-		//printf("%c\n", numEncode );
 		charPtr++;
 
 	}
@@ -47,8 +50,8 @@ int main(void)
 	//all the encrypted message
 	for(int k = 0; k < strlen(encodedMessage); k++)
 	{
-		printf("hey");
-		printf("%c\n", encodedMessage[k]);
+		//each number representation
+		//printf("%c\n", encodedMessage[k]);
 	}
 
 	char buffer[10];
@@ -57,20 +60,64 @@ int main(void)
 	//converts integer to string
 	snprintf(buffer, 10, "%d", value);
 	
-	blocker(encodedMessage);
-	//printf("%s",buffer);
-	//printf("%c", *charPtr);
+	int * enc_ptr;	
+	enc_ptr = blocker(encodedMessage);
 
-	
+
+       // for( int l = 0; l < int_blk_size ;  )
+        //{
+          //a =  (int) pow(int_blocks[i],3) % (int) 2537;
+          //printf("%d\n" , i);
+	  //i+=1;
+      int l = 0;
+      while(l < int_blk_size)
+      {
+          printf("Encrypted Values: %d\n" , enc_ptr[l] );
+	  l++;
+      }
+
+          //enc = (double) pow((double)int_blocks[i],13.0) % 2537.0;
+          //printf("%i\n",enc);
+
+        //}
+        
 
 	//for every character encode it
 	return 0;
 }
 
-void  blocker (char* toBlock)
+int gcd(int a,int b)
 {
+  while(a!=b)
+  {
+        if(a > b)
+            a -= b;
+        else
+            b -= a;
+  }
+
+  return a;
+
+}
+
+int * blocker (char* toBlock)
+{
+	
+	double n=p*q;
+	double totient = (p-1)*(q-1);
+
+	//printf("GCD = %d",a);
+	//Per the RSA Cryptosystem GCD of e and totient has to be 1
+        if(gcd(e,totient)!= 1)
+        {
+		printf("r and totient and gcd are not 1. Exiting...");
+		exit(0);
+	}     
+        //printf("bruhhhhhhh  %i\n",bruh);
+
+	
 	int elements_cnt = strlen(toBlock);//sizeof(toBlock) / sizeof(toBlock[0]);
-	printf("toBlcokk\n");
+	//printf("toBlcokk\n");
 	int block_size = 4;
 	int num_blocks = 10;
 	char arr[num_blocks][block_size];
@@ -102,10 +149,18 @@ void  blocker (char* toBlock)
 	int str_cnt = 0;
 	int full = 0;
 	printf("start\n");
+	//int int_blocks[10];
+	//int int_blk_size = 0;
 	for(int ii = 0; ii < num_blocks; ii++)
 	{
+	   printf("block start \n");
+	   char buffer[4] = {0};
+	   //buffer[0] = '\0';
 	   for(int jj =0; jj < block_size; jj++)
 	   {	
+
+		buffer[jj] = arr[ii][jj]; 
+		//printf("block start \n");	
 		printf("--");
 		printf("%c\n", arr[ii][jj]);
 		str_cnt++;
@@ -117,12 +172,37 @@ void  blocker (char* toBlock)
 		}
 
 	   }
+	   int int_val = atoi(buffer);
+	   int_blocks[ii] = int_val;
+	   printf("int_val is %i\n",int_val);
 	   if (full == 1)
+	   {
+		int_blk_size=ii+1; //filled up to this point
 		break;
+	    }
 
 	}
+
 	
-	//return "a";
+	//checking int array
+	int enc = 0;
+        unsigned int a = 0;
+        static int  encrypted[10];
+	for( int i = 0; i < int_blk_size ; i++)
+	{
+          //a =  (int) pow(int_blocks[i],3) % (int) 2537;
+	  a = fmod(pow((double)int_blocks[i],e),(double) 2537); 
+	  //printf("%i\n" , int_blocks[i]);
+	  //printf("Encrypted%d\n",a);
+          encrypted[i]= a;
+          //printf("check enc %i\n", encrypted[i]);
+	  //enc = (double) pow((double)int_blocks[i],13.0) % 2537.0;
+	  //printf("%i\n",enc);
+
+	}
+
+	
+	return encrypted;
 }
 
 
